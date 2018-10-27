0ae38696b67520e26214f91b7329815cf7f0893a
hmcgough <hrmagoo@gmail.com>
2018-10-27 10:21:12 -0700

    add child Operands

--- src/AudioEngine/LogicalOperators/UnaryOperator.java
+++ src/AudioEngine/LogicalOperators/UnaryOperator.java
@@ -4,7 +4,7 @@ package AudioEngine.LogicalOperators;
  * Created by WHOmagoo on 10/27/2018.
  */
 public abstract class UnaryOperator extends Operator{
-
+    Operand child;
 
     public UnaryOperator(int presedence) {
         super(presedence);
