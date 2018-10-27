a40ad20795c72839a46312ef5e9a753cdbcbc402
hmcgough <hrmagoo@gmail.com>
2018-10-27 10:55:20 -0700

    Add ConcreteOperand class as a private child class to help with testing of logical operators

--- src/OperatorsTest/OperatorTest.java
+++ src/OperatorsTest/OperatorTest.java
@@ -1,6 +1,7 @@
 package OperatorsTest;
 
 import AudioEngine.LogicalOperators.And;
+import AudioEngine.LogicalOperators.Operand;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,6 +9,19 @@ import static org.junit.jupiter.api.Assertions.*;
  * Created by WHOmagoo on 10/27/2018.
  */
 class OperatorTest {
+    private class ConcreteOperand implements Operand{
+        boolean bool;
+
+        public ConcreteOperand(boolean bool){
+            this.bool = bool;
+        }
+
+        @Override
+        public boolean evaluate() {
+            return bool;
+        }
+    }
+
     @org.junit.jupiter.api.Test
     void getPrecedence() {
         And and = new And();
