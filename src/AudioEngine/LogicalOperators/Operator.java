35d0dce074531be7290baa56b8a9d9f5f4334890
hmcgough <hrmagoo@gmail.com>
2018-10-27 09:44:57 -0700

    Add abstract operator class for use of boolean logic.

---
+++ src/AudioEngine/LogicalOperators/Operator.java
@@ -0,0 +1,12 @@
+package AudioEngine.LogicalOperators;
+
+/**
+ * Created by WHOmagoo on 10/27/2018.
+ */
+public abstract class Operator {
+    int presedence;
+
+    public Operator(int presedence){
+        this.presedence = presedence;
+    }
+}
