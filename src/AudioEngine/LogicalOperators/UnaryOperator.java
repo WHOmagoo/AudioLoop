853f1ffc1fa011f58c7a42ad0990125099ef671b
hmcgough <hrmagoo@gmail.com>
2018-10-27 10:12:41 -0700

    Add abstract UnaryOperator class for unary operators

---
+++ src/AudioEngine/LogicalOperators/UnaryOperator.java
@@ -0,0 +1,12 @@
+package AudioEngine.LogicalOperators;
+
+/**
+ * Created by WHOmagoo on 10/27/2018.
+ */
+public abstract class UnaryOperator extends Operator{
+
+
+    public UnaryOperator(int presedence) {
+        super(presedence);
+    }
+}
